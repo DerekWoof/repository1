e62e6fb3f4fcd46a4a0bb7fac54177111f0af0f7
Your Name <you@example.com>
2017-07-12 20:30:51 +0200

    Dodanie class KLASA

---
+++ src/Klasa.java
@@ -0,0 +1,16 @@
+
+public class Klasa {
+    private int a;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA( int a ) {
+        this.a = a;
+    }
+
+    public Klasa( int a ) {
+        this.a = a;
+    }
+}
